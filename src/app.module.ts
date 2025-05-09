64c8cd5285d527bd305fec87e0c7de24eef87d6c
Miguel <miguel.suarezo@autonoma.edu.co>
2025-05-08 19:46:46 -0500

    Modules and some functionalities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [UsersModule, UsersModule, DatabaseModule, NotificationsModule, ConfigModule.forRoot({isGlobal: true}),],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+    
+    generateVerifCode(): string {
+        return Math.floor(100000 + Math.random() * 900000).toString();
+    }
+
+
+
+}
